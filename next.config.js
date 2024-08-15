c25a2785507a1c57d881a0cbca5375f087e16b11
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-15 14:21:40 +0200

    changes in config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: 'admin.gardenbros.sk',
-          port: '1337',
           pathname: '/uploads/**',
         },
         {
